261ef50f061a8724a9f6e8ba6b9fcd98598c53b6
liqiniuniu <1578707446@qq.com>
2020-02-12 17:19:06 +0800

    push

--- git-push.sh
+++ git-push.sh
@@ -1,1 +1,1 @@
-rm allPages.zip && cp /root/front/allPages/allPages.zip . && git add . && git commit -m "push" && git push -u origin master
+rm allPages.zip && cp /root/front/allPages/allPages.zip . && git add . && git commit -m "push" && git push -u origin master /liqiniuniu
